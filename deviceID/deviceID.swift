598d6db59fd07ed0f6bf4a9154634c456f9ddc54
Yaniv Assaf <epiccodedev@gmail.com>
2023-07-10 15:02:08 +0300

    V0.05

--- deviceID/deviceID.swift
+++ deviceID/deviceID.swift
@@ -112,41 +112,41 @@ class Identification: Codable {
     func setKey(key: String) {
         self.token = key
     }
-    
-    func keychainSave(_ data: Data, service: String, account: String) {
-        
-        // Create query
-        let query = [
-            kSecValueData: data,
-            kSecClass: kSecClassGenericPassword,
-            kSecAttrService: service,
-            kSecAttrAccount: account,
-        ] as CFDictionary
-        
-        // Add data in query to keychain
-        let status = SecItemAdd(query, nil)
-        
-        if status != errSecSuccess {
-            // Print out the error
-            print("Error: \(status)")
-        }
-    }
-    
-    func keychainRead(service: String, account: String) -> Data? {
-        
-        let query = [
-            kSecAttrService: service,
-            kSecAttrAccount: account,
-            kSecClass: kSecClassGenericPassword,
-            kSecReturnData: true
-        ] as CFDictionary
-        
-        var result: AnyObject?
-        SecItemCopyMatching(query, &result)
-        
-        return (result as? Data)
-    }
 
+}
+
+func keychainSave(_ data: Data, service: String, account: String) {
+    
+    // Create query
+    let query = [
+        kSecValueData: data,
+        kSecClass: kSecClassGenericPassword,
+        kSecAttrService: service,
+        kSecAttrAccount: account,
+    ] as CFDictionary
+    
+    // Add data in query to keychain
+    let status = SecItemAdd(query, nil)
+    
+    if status != errSecSuccess {
+        // Print out the error
+        print("Error: \(status)")
+    }
+}
+
+func keychainRead(service: String, account: String) -> Data? {
+    
+    let query = [
+        kSecAttrService: service,
+        kSecAttrAccount: account,
+        kSecClass: kSecClassGenericPassword,
+        kSecReturnData: true
+    ] as CFDictionary
+    
+    var result: AnyObject?
+    SecItemCopyMatching(query, &result)
+    
+    return (result as? Data)
 }
 
 final class deviceID {
